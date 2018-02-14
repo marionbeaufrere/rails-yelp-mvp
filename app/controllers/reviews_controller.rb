edef690d829a7f016b207c37b52650ac61e86add
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-14 12:56:37 +0100

    fixed add a review issue plus added if else

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,16 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    if @review.save
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: (0..5) }
-   validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { only_integer: true }
 end
