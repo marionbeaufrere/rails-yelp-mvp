94457d51e7754c76b05241bab56e84ae72b4c34d
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-14 12:29:34 +0100

    added 6 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [ :index, :new, :create ]
+  resources :restaurants, only: [ :index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create ]
   end
-  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Alicheur',
+    address:      'Rue Oberkampf',
+    phone_number: '12345678',
+    category:     'french'
+  },
+  {
+    name:         'Mamma Roma',
+    address:      'Rue Oberkampf',
+    phone_number: '23456789',
+    category:     'italian'
+  },
+  {
+    name:         'Thanh cuisine',
+    address:      'Villa Gaudelet',
+    phone_number: '34567891',
+    category:     'chinese'
+  },
+  {
+    name:         'Lestaminet',
+    address:      'Villa Gaudelet',
+    phone_number: '45678910',
+    category:     'french'
+  },
+  {
+    name:         'Bonhomie',
+    address:      'Starsbourg Saint Denis',
+    phone_number: '56789101',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
